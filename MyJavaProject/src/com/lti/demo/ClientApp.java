232ef242d92b9c035d0401e6e27c5ff7db64dd36
KinjalMhatre <Kinjal1>
2020-11-19 18:00:10 +0530

    my second git push

--- MyJavaProject/src/com/lti/demo/ClientApp.java
+++ MyJavaProject/src/com/lti/demo/ClientApp.java
@@ -5,6 +5,8 @@ public class ClientApp {
 	public static void main(String[] args) {
 		Person p = new Person(101, "Jay", 90.00);
 		System.out.println(p);
+		Person p1 = new Person(102, "Vijay", 80.00);
+		System.out.println(p1);
 	
 	}
 }
